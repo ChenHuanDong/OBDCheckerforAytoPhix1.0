9b81c218f7e9e4b3942753555698f96636c418bf
CHD <3546975868@qq.com>
2017-08-01 13:57:05 +0800

    8.1 切换过场动画

--- app/src/main/java/com/example/administrator/obdcheckerforaytophix10/MainActivity.java
+++ app/src/main/java/com/example/administrator/obdcheckerforaytophix10/MainActivity.java
@@ -6,9 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 import android.widget.RadioButton;
 
 import com.example.administrator.obdcheckerforaytophix10.main.MainOBDFragment;
@@ -16,8 +14,14 @@ import com.example.administrator.obdcheckerforaytophix10.main.MainPersionalFragm
 import com.example.administrator.obdcheckerforaytophix10.main.MainSpecialFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    //底部三个RadioButton
     private RadioButton mRadioButton_obd, mRadioButton_special, mRadioButton_persional;
+    //三个页面的Fragment
+    private MainOBDFragment mOBDFragment;
+    private MainSpecialFragment mSpecialFragment;
+    private MainPersionalFragment mPersionalFragment;
+    //判断现在是在哪一页
+    private int page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +29,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //隐藏状态栏
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //隐藏表图兰
+        //隐藏标题栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //初始化
         initView();
+        //设置第一个按钮不可点击
+        mRadioButton_obd.setClickable(false);
 
 
     }
@@ -42,33 +49,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mRadioButton_obd.setOnClickListener(this);
         mRadioButton_special.setOnClickListener(this);
         mRadioButton_persional.setOnClickListener(this);
-        //设置第一个OBD按钮已经被点击
-        mRadioButton_obd.performClick();
+        //Fragment初始化
+        mOBDFragment = new MainOBDFragment();
+        mSpecialFragment = new MainSpecialFragment();
+        mPersionalFragment = new MainPersionalFragment();
+        //初始页面设置为OBD页面
+        change_fragment_noanim(mOBDFragment);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_main_obd:
-                change_fragment(new MainOBDFragment());
+                //第一个不可点后两个可以点击
+                mRadioButton_obd.setClickable(false);
+                mRadioButton_special.setClickable(true);
+                mRadioButton_persional.setClickable(true);
+
+                change_fragment_obd(mOBDFragment, R.anim.slide_left_mid, R.anim.slide_mid_right);
+
+                //设置页数在第一页
+                page = 1;
                 break;
             case R.id.btn_main_special:
-                change_fragment(new MainSpecialFragment());
+                //第二个不可点后两个可以点击
+                mRadioButton_obd.setClickable(true);
+                mRadioButton_special.setClickable(false);
+                mRadioButton_persional.setClickable(true);
+
+                if (page == 1) {
+                    change_fragment_obd(mSpecialFragment, R.anim.slide_right_mid, R.anim.slide_mid_left);
+                }
+                if (page == 3) {
+                    change_fragment_obd(mSpecialFragment, R.anim.slide_left_mid, R.anim.slide_mid_right);
+                }
+
+                //设置页数在第二页
+                page = 2;
                 break;
             case R.id.btn_main_persional:
-                change_fragment(new MainPersionalFragment());
+                //第三个不可点后两个可以点击
+                mRadioButton_obd.setClickable(true);
+                mRadioButton_special.setClickable(true);
+                mRadioButton_persional.setClickable(false);
+
+                change_fragment_obd(mPersionalFragment, R.anim.slide_right_mid, R.anim.slide_mid_left);
+
+                //设置页数在第三页
+                page = 3;
                 break;
         }
     }
-    //替换fragment方法
-    public void change_fragment (Fragment fragment){
+
+    //替换fragment方法 带动画
+    private void change_fragment_obd(Fragment fragment, int anim_in, int anim_out) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace( R.id.frame_main_replace , fragment);
+        //设置动画  必须要在add  remove  replace 之前调用
+        transaction.setCustomAnimations(anim_in, anim_out);
+        transaction.replace(R.id.frame_main_replace, fragment);
+        //加入到BackStack
+        transaction.addToBackStack(null);
         transaction.commit();
     }
 
-
+    //没有动画的切换fragment
+    public void change_fragment_noanim(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame_main_replace, fragment);
+        transaction.commit();
+    }
 
 
 }
--- app/src/main/java/com/example/administrator/obdcheckerforaytophix10/main/MainOBDFragment.java
+++ app/src/main/java/com/example/administrator/obdcheckerforaytophix10/main/MainOBDFragment.java
@@ -2,17 +2,24 @@ package com.example.administrator.obdcheckerforaytophix10.main;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.administrator.obdcheckerforaytophix10.R;
+import com.example.administrator.obdcheckerforaytophix10.tool.LogUtil;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainOBDFragment extends Fragment {
+public class MainOBDFragment extends Fragment implements View.OnClickListener {
+
+    private TextView tv_title;
+    private Button btn_title;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +27,37 @@ public class MainOBDFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main_obd, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
 
 
+    private void initView(View view) {
+        tv_title = view.findViewById(R.id.obd_tv_title_connect);
+        btn_title = view.findViewById(R.id.obd_btn_title_connect);
+        tv_title.setOnClickListener(this);
+        btn_title.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.obd_tv_title_connect:
+                LogUtil.fussenLog().d("点击了tv");
+                break;
+            case R.id.obd_btn_title_connect:
+                LogUtil.fussenLog().d("点击btn");
+                break;
+        }
+    }
 }
